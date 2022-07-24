21a90783c8028dd73c00fcad5f02fbac3c8dd9d9
ptrosek <80350895+ptrosek@users.noreply.github.com>
2022-07-24 13:42:35 +0200

    extending layout

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 
 Session(app)
+user = flask.session.get("user_id")
 
 @app.route("/")
 def test():
-    return flask.render_template("layout.html")
+    return flask.render_template("layout.html", user)
