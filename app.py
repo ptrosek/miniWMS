75c00270211aa2d805a9840652288bf55953b8eb
ptrosek <80350895+ptrosek@users.noreply.github.com>
2022-08-01 00:40:22 +0200

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import flask
 import os
 from flask_session import Session
@@ -5,6 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import sqlalchemy as sa
 from sqlalchemy.ext.automap import automap_base
 from helpers import login_required
+
 app = flask.Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -13,22 +15,25 @@ app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
 # db setup
+# get login data for mysql server 
 DB_USER = os.environ.get('DB_USER_MWS')
 # print(DB_USER)
 DB_PASS = os.environ.get('DB_PASS_MWS')
 # print(DB_PASS)
 txt = 'mysql://{duser}:{dpass}@localhost/miniwms'.format(duser = DB_USER, dpass = DB_PASS)
-engine = sa.create_engine(txt, echo=False, future=True)
+engine = sa.create_engine(txt, echo=True, future=True)
 meta = sa.MetaData()
-# conn = engine.connect()
 Base = automap_base()
 Base.prepare(engine)
 dbs = sa.orm.Session(engine)
 # Show the metadata
 # for t in Base.metadata.sorted_tables:
+#           conn = engine.connect()
 #           print(f"\nTable {t.name}:")
 #           for c in t.columns:
             #   print(f"{c} ({c.type})")
+#           conn.close()
+# setting allisases for tables
 good_type = Base.classes.good_type
 category = Base.classes.category
 operation_type = Base.classes.operation_type
@@ -46,10 +51,28 @@ receipt = Base.classes.receipt
 type__category = Base.classes.type__category
 record = Base.classes.record
 record_ops = Base.classes.record_ops
-t = dbs.query(good_type).all()
-for r in t:
-     print(r.name)
-print(t)
+
+conn = engine.connect()
+# test query
+# conn = engine.connect()
+# t = dbs.query(good_type).all()
+# for r in t:
+#      print(r.name)
+# print(t)
+# conn.close()
+
+# create admin user 
+def create_admin():
+    admin = user(
+        id = 1,
+        name = "admin",
+        hash = generate_password_hash("admin", method='pbkdf2:sha256', salt_length=8)
+    )
+    dbs.add(admin)
+    dbs.commit()
+    return
+# create_admin()
+
 @app.route("/")
 @login_required
 def index_worker():
@@ -59,27 +82,31 @@ def index_worker():
 def login():
     """Log user in"""
     # Forget any user_id
-    flask.session.clear()
+    flask.session["user_id"] = None
     # User reached route via POST (as by submitting a form via POST)
     if flask.request.method == "POST":
-
         # Ensure username was submitted
-        if not flask.request.form.get("username"):
-            return flask.abort(400)
-
+        if not flask.request.form.get("uid"):
+            flask.flash("username not entered")
+            return flask.redirect("/login")
         # Ensure password was submitted
-        elif not flask.request.form.get("password"):
-            return flask.abort(400)
+        elif not flask.request.form.get("pass"):
+            flask.flash("password not entered")
+            return flask.redirect("/login")
 
         # Query database for username
-        rows = 123
-
+        rows = dbs.query(user).filter_by(name=flask.request.form.get("uid")).first()
+        # print(rows.name)
+        # print(rows.hash)
+        # print(generate_password_hash("admin", method='pbkdf2:sha256', salt_length=8))
+        # print(check_password_hash(rows.hash, flask.request.form.get("pass")))
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], flask.request.form.get("password")):
-            return flask.abort(400)
+        if not check_password_hash(rows.hash, flask.request.form.get("pass")):
+              flask.flash("password and user name dont match")
+              return flask.render_template("login.html")
 
-        # Remember which user has logged in
-        flask.session["user_id"] = rows[0]["id"]
+        # # Remember which user has logged in
+        flask.session["user_id"] = rows.id
 
         # Redirect user to home page
         return flask.redirect("/")
@@ -87,4 +114,14 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return flask.render_template("login.html")
-        
+@app.route('/logout')
+def logout():
+    flask.session["user_id"] = None
+    return flask.redirect("/login")
+@app.route("/category", methods=["GET","POST"])
+def category():
+    if flask.request.method == "POST":
+        flask.flash("category created")
+        return flask.render_template("index.html")
+    else:
+        return flask.render_template("category.html")
--- db/miniwms.sql
+++ db/miniwms.sql
@@ -388,7 +388,7 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL UNIQUE,
   `hash` varchar(1000) NOT NULL,
   `mail` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
