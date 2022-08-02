c517d2a6cbdba5fd420971dca49848ab4980f14b
ptrosek <80350895+ptrosek@users.noreply.github.com>
2022-08-02 18:14:01 +0200

    further work on "other actions"

--- app.py
+++ app.py
@@ -19,18 +19,25 @@ DB_USER = os.environ.get('DB_USER_MWS')
 # print(DB_USER)
 DB_PASS = os.environ.get('DB_PASS_MWS')
 # print(DB_PASS)
-txt = 'mysql://{duser}:{dpass}@localhost/miniwms'.format(duser = DB_USER, dpass = DB_PASS)
-engine = sa.create_engine(txt, echo=True, future=True)
-meta = sa.MetaData()
-Base = automap_base()
-Base.prepare(engine)
-# Show the metadata
+
+# creating sqlalchemy connection string and connecting the engine
+try:
+    txt = 'mysql://{duser}:{dpass}@localhost/miniwms'.format(duser = DB_USER, dpass = DB_PASS)
+    engine = sa.create_engine(txt, echo=True, future=True)
+    meta = sa.MetaData()
+    Base = automap_base()
+    Base.prepare(engine)
+except:
+    flask.abort(500)
+
+# Show the metadata(used to create tables.txt)
 # for t in Base.metadata.sorted_tables:
 #           conn = engine.connect()
 #           print(f"\nTable {t.name}:")
 #           for c in t.columns:
             #   print(f"{c} ({c.type})")
 #           conn.close()
+
 # setting allisases for tables
 good_type = Base.classes.good_type
 category = Base.classes.category
@@ -58,9 +65,11 @@ record_ops = Base.classes.record_ops
 # print(t)
 # conn.close()
 
+# setting dbs later used to work with db data
+dbs = sa.orm.Session(engine)
+
 # create admin user 
 def create_admin():
-    dbs = sa.orm.Session(engine)
     dbs.begin()
     admin = user(
         id = 1,
@@ -94,12 +103,13 @@ def login():
             flask.flash("password not entered")
             return flask.redirect("/login")
         # Query database for username
-        dbs = sa.orm.Session(engine)
+        dbs.begin()
         rows = dbs.query(user).filter_by(name=flask.request.form.get("uid")).first()
         # print(rows.name)
         # print(rows.hash)
         # print(generate_password_hash("admin", method='pbkdf2:sha256', salt_length=8))
         # print(check_password_hash(rows.hash, flask.request.form.get("pass")))
+
         # Ensure username exists and password is correct
         if not check_password_hash(rows.hash, flask.request.form.get("pass")):
               flask.flash("password and user name dont match")
@@ -116,10 +126,13 @@ def login():
         return flask.render_template("login.html")
 @app.route('/logout')
 def logout():
+    # set the user id to none making login_required return false
     flask.session["user_id"] = None
     return flask.redirect("/login")
 @app.route("/category", methods=["GET","POST"])
+@login_required
 def cat():
+    # check if the required elements were included
     if flask.request.method == "POST":
         if (not flask.request.form.get("name")) or (len(flask.request.form.get("name")) > 255):
             flask.flash("wrong name")
@@ -127,8 +140,9 @@ def cat():
         elif len(flask.request.form.get("desc")) > 1000:
             flask.flash("max amount of characters in description is 1000")
             return flask.redirect("/category")
+        # transaction to add form data into the db
         try:
-            dbs = sa.orm.Session(engine)
+            dbs.begin()
             q = category(
                 name = flask.request.form.get("name"),
                 description = flask.request.form.get("desc")
@@ -143,30 +157,34 @@ def cat():
     else:
         return flask.render_template("category.html")
 @app.route("/gtc", methods=["GET","POST"])
+@login_required
 def gtc():
+    # check if the required elements were included
     if flask.request.method == "POST":
         if not flask.request.form.get("name"):
             flask.flash("name input blank")
             return flask.redirect("/")
+        # transaction to input form data into db
         try:
-            dbs = sa.orm.Session(engine)
             if flask.request.form.get("ean"):
                 ean = flask.request.form.get("ean")
             else:
-                ean = "NULL"
+            # if the data was not inputed set variable to sql NULL
+                ean = sa.sql.null()
             if flask.request.form.get("size"):
                 size = flask.request.form.get("size")
             else:
-                size = "NULL"
+                size = sa.sql.null()
             if flask.request.form.get("weight"):
                 weight = flask.request.form.get("weight")
             else:
-                weight = "NULL"
-            if flask.request.form.get("pt"):
+                weight = sa.sql.null()
+            if flask.request.form.get("pt") != 'None':
                 pt = flask.request.form.get("pt")
             else:
-                pt = "NULL"
-            flask.request.form.getlist("cc")
+                pt = sa.sql.null()
+            # flask.request.form.getlist("cc")
+            dbs.begin()
             q = good_type(
                  name = flask.request.form.get("name"),
                  ean = ean,
@@ -175,14 +193,25 @@ def gtc():
                  package_type_gt = pt
                  )
             dbs.add(q)
+            # inputing data into db without needing to commit it to be able to retrive q.id
+            dbs.flush()
+            if flask.request.form.get("cc"):
+                for i in range(len(flask.request.form.getlist("cc"))):
+                    p = type__category(
+                        categoryid = flask.request.form.getlist("cc")[i],
+                        type_id = q.id
+                    )
+                    dbs.add(p)
+                    # print(p.type_id)
             dbs.commit()
         except:
-            flask.flash("name input blank")
+            flask.flash("error in database")
             return flask.redirect("/")
         flask.flash("good type created")
         return flask.redirect("/")
     else:
-        dbs = sa.orm.Session(engine)
+        # get data from the db saved as a list to make it iterable in jinjja and viewed as select
+        dbs.begin()
         types = dbs.scalars(sa.select(package_type))
         listt = []
         for type in types:
@@ -195,3 +224,41 @@ def gtc():
         dbs.commit()
         # print(list)
         return flask.render_template("gtc.html", types=listt, cats=listc)
+@app.route("/org",methods=["GET", "POST"])
+@login_required
+def org():
+    # works the same way as POST part of gtc
+    if flask.request.method == "POST":
+        if not flask.request.form.get("name"):
+            flask.flash("name not inputed")
+            return flask.redirect("/org")
+        name = flask.request.form.get("name")
+        if not flask.request.form.get("name"):
+            flask.flash("mail not inputed")
+            return flask.redirect("/org")
+        mail = flask.request.form.get("mail")
+        if flask.request.form.get("bi"):
+            bi = flask.request.form.get("bi")
+        else:
+            bi = sa.sql.null()
+        if flask.request.form.get("address"):
+            adr = flask.request.form.get("address")
+        else:
+            adr = sa.sql.null()
+        try:
+            dbs.begin()
+            q = outside_org(
+                name = name,
+                mail = mail,
+                bank_info = bi,
+                addres = adr
+            )
+            dbs.add(q)
+            dbs.commit()
+        except:
+            flask.flash("database error")
+            return flask.redirect("/")
+        flask.flash("org created")
+        return flask.redirect("/")
+    else:
+        return flask.render_template("org.html")
